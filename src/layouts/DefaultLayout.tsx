afef22aad8db569bc2ab820bb25668bb86b59052
Daniella Dutra <dani.ldutra@gmail.com>
2022-07-30 15:40:56 -0300

    feature: layout construction

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
+import { LayoutContainer } from './styles';
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
       {/* component do react-router-dom que permite manter um espaço
       para ser inserido um conteúdo para transicionar. */}
-    </div>
+    </LayoutContainer>
   );
 }
