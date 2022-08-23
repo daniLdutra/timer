32e5836999c46478eee7272fdc9ad53557c8584d
Daniella Dutra <dani.ldutra@gmail.com>
2022-08-23 11:34:07 -0300

    refactor: applied logic to complete the timer cycle

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,7 +20,7 @@ const formValidationSchema = zod.object({
   // owner: zod.string().optional(), exemplo
   minutesAmount: zod
     .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .min(1, 'O ciclo precisa ser de no mínimo 5 minutos')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 });
 
@@ -37,6 +37,7 @@ interface Cycle {
   minutesAmount: number;
   startDate: Date;
   interruptedDate?: Date;
+  finishedDate?: Date;
 }
 
 export function Home() {
@@ -55,21 +56,39 @@ export function Home() {
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
   useEffect(() => {
     let interval: number;
 
     if (activeCycle) {
       interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
-        );
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+      )
+      if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+          )
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference)
+        }
       }, 1000);
     }
 
     return () => {
       clearInterval(interval);
     };
-  }, [activeCycle]);
+  }, [activeCycle, totalSeconds, activeCycleId]);
 
   function handleCreateNewCicle(data: NewFormData) {
     const id = String(new Date().getTime());
@@ -89,8 +108,8 @@ export function Home() {
   }
 
   function handleInterruptCycle() {
-    setCycles(
-      cycles.map((cycle) => {
+    setCycles((state) =>
+      state.map((cycle) => {
         if (cycle.id === activeCycleId) {
           return { ...cycle, interruptedDate: new Date() };
         } else {
@@ -101,9 +120,6 @@ export function Home() {
     setActiveCycleId(null)
   }
 
-  //variável converte o número de minutos inserido no ciclo pelo usuário em segundos.
-  // verifica-se se o ciclo está ativo, variável será o número de minutos do ciclo * 60 (1 minuto = 60 seg), se não tiver um ciclo ativo a variável será 0
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   //calcula-se o total de segundos pelo que já passou
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
@@ -125,6 +141,7 @@ export function Home() {
 
   const task = watch('task'); //watch: permite monitorar o formulário = "controller"
   const isSubmitDisable = !task;
+
 
   return (
     <HomeContainer>
@@ -153,7 +170,7 @@ export function Home() {
             placeholder="00"
             disabled={!!activeCycle}
             step={5}
-            min={5}
+            min={1}
             max={60}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
