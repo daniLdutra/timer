73285ea5220b1f0f6bbf9fd8370ea8679fd60669
Daniella Dutra <dani.ldutra@gmail.com>
2022-08-31 14:49:43 -0300

    feature: marking cycle as finished

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -63,6 +63,19 @@ export function CyclesContextProvider({
           activeCycleId: null,
         };
       }
+      if(action.type === 'MARK_CURRENT_CYCLE_AS_FINISHED'){
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        };
+      }
       return state;
     },
     {
@@ -85,16 +98,6 @@ export function CyclesContextProvider({
         activeCycleId,
       },
     });
-    //   setCycles((state) =>
-    //     state.map((cycle) => {
-    //       if (cycle.id === activeCycleId) {
-    //         return { ...cycle, finishedDate: new Date() };
-    //       } else {
-    //         return cycle;
-    //       }
-    //     })
-    //   );
-    // }
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -113,7 +116,6 @@ export function CyclesContextProvider({
         newCycle,
       },
     });
-    // setCycles((state) => [...state, newCycle]);
     setAmountSecondsPassed(0);
   }
 
